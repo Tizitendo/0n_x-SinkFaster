b70f7dbf93a5770b959cd83fb5b10cb6a2efbe08
Onyx <50289874+Tizitendo@users.noreply.github.com>
2024-11-03 20:28:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Onyx-SinkFaster
+- Hold down to sink faster
+
+## Special Thanks To
+* The Return Of Modding Discord
+
+## Contact
+For questions or bug reports, you can find us in the [RoRR Modding Server](https://discord.gg/VjS57cszMq) @Onyx
---
+++ main.lua
@@ -0,0 +1,26 @@
+log.info("Successfully loaded " .. _ENV["!guid"] .. ".")
+params = {}
+mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto()
+mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "table" and v.tomlfuncs then Toml = v end end 
+end)
+
+LastWet = -1.0
+player = nil
+Initialize(function()
+    Callback.add("onPlayerInit", "SinkFaster-onPlayerInit", function()
+        player = Player.get_client()
+        LastWet = -1
+    end)
+    Callback.add("onPlayerStep", "SinkFaster-onPlayerStep", function()
+        if player.value.wet > LastWet then
+            LastWet = player.value.wet
+            if player.value.ropeDown == 1.0 then
+                player.value.pGravity1 = 1.0
+            else
+                player.value.pGravity1 = player.value.pGravity1_base
+            end
+        else
+            player.value.pGravity1 = player.value.pGravity1_base
+        end
+    end)
+end)
