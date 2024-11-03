8e23427ba347db91c8281935ad14dafbb04dcca1
Onyx <50289874+Tizitendo@users.noreply.github.com>
2024-11-03 20:55:17 +0100

    fixed annoying geyser interaction

--- main.lua
+++ main.lua
@@ -1,10 +1,16 @@
 log.info("Successfully loaded " .. _ENV["!guid"] .. ".")
 params = {}
 mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto()
-mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "table" and v.tomlfuncs then Toml = v end end 
+mods.on_all_mods_loaded(function()
+    for k, v in pairs(mods) do
+        if type(v) == "table" and v.tomlfuncs then
+            Toml = v
+        end
+    end
 end)
 
 LastWet = -1.0
+BlockSinking = 0
 player = nil
 Initialize(function()
     Callback.add("onPlayerInit", "SinkFaster-onPlayerInit", function()
@@ -14,7 +20,7 @@ Initialize(function()
     Callback.add("onPlayerStep", "SinkFaster-onPlayerStep", function()
         if player.value.wet > LastWet then
             LastWet = player.value.wet
-            if player.value.ropeDown == 1.0 then
+            if player.value.ropeDown == 1.0 and player.value.pVspeed > 0 then
                 player.value.pGravity1 = 1.0
             else
                 player.value.pGravity1 = player.value.pGravity1_base
